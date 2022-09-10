f0e0bc61cd606f1117e8977778fcf237d781c6b8
Olasubomi <subomioyetoso@gmail.com>
2022-09-10 08:51:27 +0000

    adv task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the last part of a quote in the stderror
+ *
+ * Return 1 if successful
+ * 
+ * */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	return (1)
+}
