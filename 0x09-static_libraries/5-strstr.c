f3117f1ea2a9a02bbb6e11074dcc5f26b8c2bdd3
Olasubomi <subomioyetoso@gmail.com>
2022-10-01 22:27:29 +0000

    task

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *  _strstr - locates a substring
+ *
+ *  @haystack: the longer string to search
+ *  @needle: the first occurrence of the substring
+ *
+ *  Return: a pointer beg of substring or @Null if it not foound.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+			if (!*needle)
+			{
+				return (result);
+			}
+			needle = fneedle;
+			result++;
+			haystack = result;
+	}
+	return (0);
+}
