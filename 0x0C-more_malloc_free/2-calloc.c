a7887953835c7c4a5c8a4d5fc728348c6b21f440
Subomi <subomioyetoso@gmail.com>
2022-10-06 21:47:22 -0700

    First two

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_noncatcreate_array -
+ * 
+ * @size: size of array
+ *
+ * @c: char to assign
+ * Description:
+ *
+ * Return:
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	return (0);
+}
