1c1f58cf43622f14a8e6c97a0642d0348f0a214c
Olasubomi <subomioyetoso@gmail.com>
2022-10-10 15:50:46 +0000

    First task on 0-Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * struct dog -This is a struct element with the below data
+ * char, int
+ * name, owner, age.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	int age;
+}
